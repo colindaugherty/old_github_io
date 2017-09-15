9ba3ba744b31b649ba3ff7c9d8fb640c73b25ce3
Colin Daugherty <colindaugherty@Colins-MacBook-Pro.local>
2017-09-15 03:52:44 -0400

    boy howdy thats a lot of additions

--- script/script.js
+++ script/script.js
@@ -1,1 +1,29 @@
-// Little functions and other helpful stuff
+function ready(fn) {
+    if (document.readyState != 'loading'){
+      fn();
+    } else {
+      document.addEventListener('DOMContentLoaded', fn);
+    }
+  }
+
+var COLINSITE = (function() {
+
+    function main() {
+        var currentYear;
+
+        currentYear = new Date().getFullYear();
+        $('#year').text(currentYear);
+        console.log(currentYear);
+    }
+
+    function init() {
+        main()
+      }
+
+    return {
+        init : init,
+      };
+
+})();
+
+ready(COLINSITE.init);
